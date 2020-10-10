ccfbfc715802731ff3c4bb5d320cf7de745c84a1
Lexi Keen <lexikeen@Lexis-MacBook-Air.local>
2020-10-10 12:25:28 -0700

    adding serve

--- site/src/index.js
+++ site/src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const handler = require("serve-handler");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+  return handler(req, res);
+})
+
+server.listen(3000, () => {
+  console.log("listening on port 3000");
+})
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
