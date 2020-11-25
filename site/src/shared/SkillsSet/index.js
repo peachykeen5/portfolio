b93c09ea5e6fae7a838cd4842ff4281622e47f1d
Lexi Keen <lexikeen@Lexis-MacBook-Air.local>
2020-11-24 16:53:20 -0800

    updating skills

--- site/src/shared/SkillsSet/index.js
+++ site/src/shared/SkillsSet/index.js
@@ -1,52 +1,63 @@
 export const skills = [
-    [
-        {
-            label: "React",
-            progress: 70,
-        },
-        {
-            label: "React Native",
-            progress: 70
-        }
-    ],
-    [
-        {
-            label: "HTML/CSS",
-            progress: 90,
-        },
-        {
-            label: "Redux",
-            progress: 25
-        }
-    ],
-    [
-        {
-            label: "Javascript",
-            progress: 70,
-        },
-        {
-            label: "Node",
-            progress: 45
-        }
-    ],
-    [
-        {
-            label: "Gatsby",
-            progress: 50,
-        },
-        {
-            label: "Firebase",
-            progress: 35
-        }
-    ],
-    [
-        {
-            label: "Netlify CMS",
-            progress: 80,
-        },
-        {
-            label: "Web Design",
-            progress: 60,
-        }
-    ],
-  ];
+  [
+    {
+      label: "React",
+      progress: 70,
+    },
+    {
+      label: "React Native",
+      progress: 70,
+    },
+  ],
+  [
+    {
+      label: "Javascript",
+      progress: 70,
+    },
+
+    {
+      label: "TypeScript",
+      progress: 60,
+    },
+  ],
+  [
+    {
+      label: "Redux",
+      progress: 35,
+    },
+    {
+      label: "Firebase",
+      progress: 35,
+    },
+  ],
+  [
+    {
+      label: "HTML/CSS",
+      progress: 90,
+    },
+    {
+      label: "Node",
+      progress: 45,
+    },
+  ],
+  [
+    {
+      label: "Gatsby",
+      progress: 50,
+    },
+    {
+      label: "Shopify",
+      progress: 35,
+    },
+  ],
+  [
+    {
+      label: "Netlify CMS",
+      progress: 80,
+    },
+    {
+      label: "Web Design",
+      progress: 60,
+    },
+  ],
+];
